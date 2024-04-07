af519dbfaf59ae720cfaba35f69d65c02133c7a9
deepshah <dshah12@students.cs.ubc.ca>
2024-04-06 20:20:07 -0700

    Dashboard

--- fitNest/src/App.jsx
+++ fitNest/src/App.jsx
@@ -4,7 +4,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Login';
 import Landing from './Landing'; // Assuming you split Landing content into a separate component
 import Register from './Register';
-import PushUps from './challenge/challenge1.jsx'
+import PushUps from './challenge/challenge1.jsx';
+import Dashboard from './Dashboard.jsx';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path = "/challenge1" element = {<PushUps />} />
+        <Route path = "/dashboard" element = {<Dashboard />} />
       </Routes>
     </div>
   );
---
+++ fitNest/src/Dashboard.jsx
@@ -0,0 +1,309 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './Dashboard.css'
+
+const Dashboard = (props) => {
+  return (
+    <div className="galileodesign-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="galileodesign-galileodesign">
+        <div className="galileodesign-depth0-frame0">
+          <div className="galileodesign-depth1-frame0">
+            <div className="galileodesign-depth2-frame0">
+              <div className="galileodesign-depth3-frame0">
+                <div className="galileodesign-depth4-frame0">
+                  <div className="galileodesign-depth5-frame0">
+                    <img
+                      src="/external/depth6frame06252-cxs1.svg"
+                      alt="Depth6Frame06252"
+                      className="galileodesign-depth6-frame0"
+                    />
+                  </div>
+                  <div className="galileodesign-depth5-frame1">
+                    <div className="galileodesign-depth6-frame001">
+                      <span className="galileodesign-text">
+                        <span>FitNest</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="galileodesign-depth3-frame1">
+                <div className="galileodesign-depth4-frame01">
+                  <div className="galileodesign-depth5-frame4"></div>
+                  <div className="galileodesign-depth5-frame001">
+                    <div className="galileodesign-depth6-frame002">
+                      <span className="galileodesign-text02">
+                        <span>Home</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame11">
+                    <div className="galileodesign-depth6-frame003">
+                      <span className="galileodesign-text04">
+                        <span>Explore</span>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame2">
+                    <div className="galileodesign-depth6-frame004">
+                      <span className="galileodesign-text06">
+                        <span>Challenges</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame2"></div>
+              </div>
+            </div>
+            <div className="galileodesign-depth2-frame1">
+              <div className="galileodesign-depth3-frame01">
+                <div className="galileodesign-depth4-frame02">
+                  <div className="galileodesign-depth5-frame002">
+                    <div className="galileodesign-depth6-frame005">
+                      <div className="galileodesign-depth7-frame0">
+                        <span className="galileodesign-text08">
+                          <span>Challenges</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame1">
+                  <div className="galileodesign-depth5-frame003">
+                    <div className="galileodesign-depth6-frame006">
+                      <span className="galileodesign-text10">
+                        <span>In Progress</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame21">
+                  <div className="galileodesign-depth5-frame004">
+                    <div className="galileodesign-depth6-frame007"></div>
+                    <div className="galileodesign-depth6-frame2">
+                      <div className="galileodesign-depth7-frame001">
+                        <div className="galileodesign-depth8-frame0">
+                          <span className="galileodesign-text12">
+                            <span>Run 20 miles this month</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame1">
+                        <div className="galileodesign-depth8-frame001">
+                          <span className="galileodesign-text14">
+                            <span>4/5</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame12">
+                    <div className="galileodesign-depth6-frame008">
+                      <div className="galileodesign-depth7-frame002">
+                        <img
+                          src="/external/depth8frame06258-9sfd.svg"
+                          alt="Depth8Frame06258"
+                          className="galileodesign-depth8-frame002"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame3">
+                  <div className="galileodesign-depth5-frame005">
+                    <div className="galileodesign-depth6-frame009"></div>
+                    <div className="galileodesign-depth6-frame1"></div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame4">
+                  <div className="galileodesign-depth5-frame006">
+                    <div className="galileodesign-depth6-frame010"></div>
+                    <div className="galileodesign-depth6-frame21">
+                      <div className="galileodesign-depth7-frame003">
+                        <div className="galileodesign-depth8-frame003">
+                          <span className="galileodesign-text16">
+                            <span>
+                              Eat 2 servings of vegetables per day (1 week left)
+                            </span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame11">
+                        <div className="galileodesign-depth8-frame004">
+                          <span className="galileodesign-text18">
+                            <span>3/4</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame13">
+                    <div className="galileodesign-depth6-frame011">
+                      <div className="galileodesign-depth7-frame004">
+                        <img
+                          src="/external/depth8frame06260-0l88.svg"
+                          alt="Depth8Frame06260"
+                          className="galileodesign-depth8-frame005"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame5">
+                  <div className="galileodesign-depth5-frame007">
+                    <div className="galileodesign-depth6-frame012"></div>
+                    <div className="galileodesign-depth6-frame11"></div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame6">
+                  <div className="galileodesign-depth5-frame008">
+                    <div className="galileodesign-depth6-frame013">
+                      <span className="galileodesign-text20">
+                        <span>Completed</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame7">
+                  <div className="galileodesign-depth5-frame009">
+                    <div className="galileodesign-depth6-frame014"></div>
+                    <div className="galileodesign-depth6-frame22">
+                      <div className="galileodesign-depth7-frame005">
+                        <div className="galileodesign-depth8-frame006">
+                          <span className="galileodesign-text22">
+                            <span>Run 20 miles this month</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame12">
+                        <div className="galileodesign-depth8-frame007">
+                          <span className="galileodesign-text24">
+                            <span>5/5</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame14">
+                    <div className="galileodesign-depth6-frame015">
+                      <div className="galileodesign-depth7-frame006">
+                        <img
+                          src="/external/depth8frame06263-rfun.svg"
+                          alt="Depth8Frame06263"
+                          className="galileodesign-depth8-frame008"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame8">
+                  <div className="galileodesign-depth5-frame010">
+                    <div className="galileodesign-depth6-frame016"></div>
+                    <div className="galileodesign-depth6-frame23">
+                      <div className="galileodesign-depth7-frame007">
+                        <div className="galileodesign-depth8-frame009">
+                          <span className="galileodesign-text26">
+                            <span>Eat 2 servings of vegetables per day</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame13">
+                        <div className="galileodesign-depth8-frame010">
+                          <span className="galileodesign-text28">
+                            <span>4/4</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame15">
+                    <div className="galileodesign-depth6-frame017">
+                      <div className="galileodesign-depth7-frame008">
+                        <img
+                          src="/external/depth8frame06264-jaqz5.svg"
+                          alt="Depth8Frame06264"
+                          className="galileodesign-depth8-frame011"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame9">
+                  <div className="galileodesign-depth5-frame011">
+                    <div className="galileodesign-depth6-frame018"></div>
+                    <div className="galileodesign-depth6-frame24">
+                      <div className="galileodesign-depth7-frame009">
+                        <div className="galileodesign-depth8-frame012">
+                          <span className="galileodesign-text30">
+                            <span>Practice yoga every day</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame14">
+                        <div className="galileodesign-depth8-frame013">
+                          <span className="galileodesign-text32">
+                            <span>7/7</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame16">
+                    <div className="galileodesign-depth6-frame019">
+                      <div className="galileodesign-depth7-frame010">
+                        <img
+                          src="/external/depth8frame06266-pcis.svg"
+                          alt="Depth8Frame06266"
+                          className="galileodesign-depth8-frame014"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="galileodesign-depth4-frame10">
+                  <div className="galileodesign-depth5-frame012">
+                    <div className="galileodesign-depth6-frame020"></div>
+                    <div className="galileodesign-depth6-frame25">
+                      <div className="galileodesign-depth7-frame011">
+                        <div className="galileodesign-depth8-frame015">
+                          <span className="galileodesign-text34">
+                            <span>Meditate for 10 minutes each day</span>
+                          </span>
+                        </div>
+                      </div>
+                      <div className="galileodesign-depth7-frame15">
+                        <div className="galileodesign-depth8-frame016">
+                          <span className="galileodesign-text36">
+                            <span>30/30</span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="galileodesign-depth5-frame17">
+                    <div className="galileodesign-depth6-frame021">
+                      <div className="galileodesign-depth7-frame012">
+                        <img
+                          src="/external/depth8frame06267-lwvw.svg"
+                          alt="Depth8Frame06267"
+                          className="galileodesign-depth8-frame017"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard

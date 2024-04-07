8314ebf2e5dce7390049c8065f54f01b5dadd119
Rshah2004 <rythemshah2004@gmail.com>
2024-04-06 18:44:56 -0700

    fixed a few erros

--- fitNest/src/challenge/challenge1.jsx
+++ fitNest/src/challenge/challenge1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from "react";
 
 const PushUps = () =>  {
-    const [dataAdded, setEntireData] = useState([]);
+    const [dataAdded, setEntireData] = useState([[{"_id":"6611eef535f840cb3d9de206","user_id":"120","challenges":[{"progress":"50%","completed":true,"pushUpsToDo":100,"_id":"6611eef535f840cb3d9de207"}],"__v":0}]]);
     useEffect(() => {
         getChallengeRequirements();
       }, []);
@@ -13,6 +13,7 @@ const PushUps = () =>  {
           const response = await fetch("http://localhost:30049/userlist");
           const data = await response.json();
           setEntireData(data);
+          console.log(data[0].challenges[0].pushUpsToDo)
           console.log(data);
         } catch (error) {
           console.error("Failed to fetch data:", error);
@@ -24,7 +25,7 @@ const PushUps = () =>  {
         <div className = "pushups" style={{ top: "100px", position: "absolute", fontWeight: "bold", fontSize: "250%", left: "40%" }}> 
             Push up challenge
             <div className='textPushups' style = {{ top: "85px", position: "absolute", fontSize: "80%", left: "-59%", width: "350%", maxWidth: "350%"}}>
-                {/* do {dataAdded[0].challenges[0].pushUpsToDo} today, and then the count keeps on increasing, till you complete your target */}
+                {/* do {dataAdded.challenges[0].pushUpsToDo} today, and then the count keeps on increasing, till you complete your target */}
             </div>
         </div>
     )
--- fitNest/src/main.jsx
+++ fitNest/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MAIN2 from './MAIN2.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MAIN2 />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <MAIN2 />
+    </BrowserRouter>
+  </React.StrictMode>
 );

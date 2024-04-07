1e8f4d2eb0cd67af4b705a11e8aa1064ef523a29
deepshah <dshah12@students.cs.ubc.ca>
2024-04-07 08:54:17 -0700

    Dashboard pf added

--- fitNest/src/Dashboard.jsx
+++ fitNest/src/Dashboard.jsx
@@ -258,6 +258,13 @@ const Dashboard = (props) => {
                         </span>
                       </div>
                     </button>
+                    <button className="galileodesign-pointbutton" onClick={()=> navigate("/explore")}>
+                      <div className="galileodesign-depth6-frame003">
+                        <span className="galileodesign-text04">
+                          <span>Explore</span>
+                        </span>
+                      </div>
+                    </button>
                     <button className="galileodesign-pointbutton">
                       <div className="galileodesign-depth6-frame003">
                         <span className="galileodesign-text04">
@@ -265,6 +272,7 @@ const Dashboard = (props) => {
                         </span>
                       </div>
                     </button>
+                    <img src="/src/images/pf.jpeg" alt="Profile" className="profile-img" />
                   </div>
                   <div className="galileodesign-depth4-frame2"></div>
                 </div>

f52954b6367c1416301919527b997c2d9c00d76f
Rshah2004 <rythemshah2004@gmail.com>
2024-04-06 22:01:38 -0700

    challenge 1

--- fitNest/src/App.jsx
+++ fitNest/src/App.jsx
@@ -4,6 +4,10 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Login';
 import Landing from './Landing'; // Assuming you split Landing content into a separate component
 import Register from './Register';
+
+import PushUps from './challenge/challenge1.jsx'
+import AnotherPage from './challenge/challenge1Video.jsx'
+
 import PushUps from './challenge/challenge1.jsx';
 import Dashboard from './Dashboard.jsx';
 
@@ -17,7 +21,11 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path = "/challenge1" element = {<PushUps />} />
+
+        <Route path = "/anotherPageChallenge1" element = {< AnotherPage />} />
+
         <Route path = "/dashboard" element = {<Dashboard />} />
+
       </Routes>
     </div>
   );

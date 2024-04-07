fb4e760dfea4e21b020d35def786f170500e1800
deepshah <dshah12@students.cs.ubc.ca>
2024-04-06 22:27:56 -0700

    new landing

--- fitNest/src/App.jsx
+++ fitNest/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
         <Route path = "/challenge1" element = {<PushUps />} />
         <Route path = "/dashboard" element = {<Dashboard />} />
       </Routes>
-    </div>
+    // </div>
   );
 };
 
